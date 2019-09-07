92f92fe34e7bb625d43337323c2d81ac7de37773
adugeen <konstantzts@gmail.com>
2019-09-07 19:23:53 +0500

    Добавление формы для вычисления

---
+++ QA/Classes/Metric.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QA.Classes
+{
+    public class Metric
+    {
+        public int Id { get; set; }
+
+        public int CriteriaId { get; set; }
+
+        public int PhaseId { get; set; }
+
+        public string Metrics { get; set; }
+
+        public Metric() { }
+    }
+}
